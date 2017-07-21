ec680c203c77b6257bcb32ae598ed568f338a02f
Ann Lee <aplee29@users.noreply.github.com>
2017-07-21 13:44:44 -0400

    Starter page mockup with basic CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="navbar">
+          <h2>Nav bar</h2>
+        </div>
+        <div className="sidebar">
+          <h2>side bar</h2>
+        </div>
+        <div className="main-content">
+          <h2>main content</h2>
+        </div>
       </div>
     );
   }
